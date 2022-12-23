be69c7d14d95721457caa7730322e0889b58bad1
Daniel Muller <mrmeku@gmail.com>
2022-12-23 11:54:49 -0700

    Add typescript support

---
+++ cypress/tests/cli_test/cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    specPattern: ["cli_test.cy.ts"],
+    supportFile: false,
+    setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser, launchOptions) => {
+        launchOptions.args.push("--disable-gpu-shader-disk-cache")
+      })
+    }
+  },
+})
