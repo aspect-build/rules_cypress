f6a649613cb8f76fc43398285c188b41f05772f6
Greg Magolan <gmagolan@gmail.com>
2023-04-08 09:16:54 -0700

    chore: update release.yaml script to use release archive

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -5,8 +5,11 @@ set -o errexit -o nounset -o pipefail
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
+# The prefix is chosen to match what GitHub generates for source archives
 PREFIX="rules_cypress-${TAG:1}"
-SHA=$(git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip | shasum -a 256 | awk '{print $1}')
+ARCHIVE="rules_cypress-$TAG.tar.gz"
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
+SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
 WORKSPACE snippet:
@@ -16,9 +19,9 @@ http_archive(
     name = "aspect_rules_cypress",
     sha256 = "${SHA}",
     strip_prefix = "${PREFIX}",
-    url = "https://github.com/aspect-build/rules_cypress/archive/refs/tags/${TAG}.tar.gz",
+    url = "https://github.com/aspect-build/rules_cypress/releases/download/${TAG}/${ARCHIVE}",
 )
 EOF
 
 awk 'f;/--SNIP--/{f=1}' e2e/workspace/WORKSPACE
-echo "\`\`\`" 
+echo "\`\`\`"
