41983b2ca5733310dc1a599aee58f2a7e4bfc37c
Greg Magolan <greg@aspect.dev>
2023-08-24 14:51:10 -0700

    cleanup exception handling

--- cypress/tests/module_test/runner.js
+++ cypress/tests/module_test/runner.js
@@ -1,26 +1,28 @@
-const cypress = require('cypress')
+const cypress = require("cypress");
 
 async function main() {
-  try {
-      const result = await cypress.run({
-          headless: true,
-      })
+  const result = await cypress.run({
+    headless: true,
+  });
 
-      // If any tests have failed, results.failures is non-zero, some tests have failed
-      if (result.failures) {
-          console.error('One or more cypress tests have failed')
-          console.error(result.message)
-          process.exit(1)
-      }
-
-      if (result.status !== 'finished') {
-          console.error('Cypress tests failed with status', result.status)
-          process.exit(2)
-      }
-  } catch (e) {
-      console.error("Cypress encountered unexpected exception. Exiting.", e)
-      process.exit(3)
+  // If any tests have failed, results.failures is non-zero, some tests have failed
+  if (result.failures) {
+    console.error("One or more cypress tests have failed");
+    console.error(result.message);
+    return 1;
   }
+
+  if (result.status !== "finished") {
+    console.error("Cypress tests failed with status", result.status);
+    return 2;
+  }
+
+  return 0;
 }
 
-main();
+main()
+  .then((code) => (process.exitCode = code))
+  .catch((e) => {
+    console.error("Cypress encountered unexpected exception. Exiting.", e);
+    process.exitCode = 3;
+  });
