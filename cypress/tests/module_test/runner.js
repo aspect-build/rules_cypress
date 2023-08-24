3a7e2a9ccaa558a701a322f4bf563cafe56ae91c
axelchauvin <116589521+axelchauvin@users.noreply.github.com>
2023-08-25 00:04:51 +0200

    fix: fix binary_state.json missing during cypress launch (#51)
    * fix binary_state.json missing during cypress launch
    
    * use copy_to_bin
    
    * cleanup exception handling
    
    ---------
    
    Co-authored-by: Greg Magolan <greg@aspect.dev>

--- cypress/tests/module_test/runner.js
+++ cypress/tests/module_test/runner.js
@@ -1,9 +1,28 @@
-const cypress = require('cypress')
+const cypress = require("cypress");
 
-cypress.run({
-  headless: true,
-}).then(result => {
-  if (result.status === 'failed') {
-    process.exit(1);
+async function main() {
+  const result = await cypress.run({
+    headless: true,
+  });
+
+  // If any tests have failed, results.failures is non-zero, some tests have failed
+  if (result.failures) {
+    console.error("One or more cypress tests have failed");
+    console.error(result.message);
+    return 1;
   }
-})
+
+  if (result.status !== "finished") {
+    console.error("Cypress tests failed with status", result.status);
+    return 2;
+  }
+
+  return 0;
+}
+
+main()
+  .then((code) => (process.exitCode = code))
+  .catch((e) => {
+    console.error("Cypress encountered unexpected exception. Exiting.", e);
+    process.exitCode = 3;
+  });
