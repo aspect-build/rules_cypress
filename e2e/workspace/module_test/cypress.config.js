7da1e0e95d8ae5308990f92dc200992dc28d02ea
Daniel Muller <mrmeku@gmail.com>
2024-05-01 09:33:52 -0600

    chore: various cleanups
    - Upgrade e2e version of cypres to 13.6.6
    - Run prettier
    - Set test timeouts to short
    - Disable cypress lifecycle for cypress

--- e2e/workspace/module_test/cypress.config.js
+++ e2e/workspace/module_test/cypress.config.js
@@ -1,13 +1,13 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     specPattern: ["*.cy.js"],
     supportFile: false,
-    setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        launchOptions.args.push("--disable-gpu-shader-disk-cache")
-      })
-    }
+    setupNodeEvents(on, _config) {
+      on("before:browser:launch", (_browser, launchOptions) => {
+        launchOptions.args.push("--disable-gpu-shader-disk-cache");
+      });
+    },
   },
-})
+});
