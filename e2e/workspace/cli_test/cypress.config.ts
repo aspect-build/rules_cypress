abe8c730fb24645b3c1edf3fe816092c0380cc2e
mrmeku <mrmeku@gmail.com>
2024-05-14 09:50:45 -0600

    chore: various cleanups (#76)
    * chore: various cleanups
    
    - Upgrade e2e version of cypres to 13.6.6
    - Run prettier
    - Set test timeouts to short
    - Disable cypress lifecycle for cypress
    
    * Set XVFB_DISPLAY_NUM to fix CI flakes

--- e2e/workspace/cli_test/cypress.config.ts
+++ e2e/workspace/cli_test/cypress.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
+
+// Set XVFB_DISPLAY_NUM to instruct cypress what port to use during CI and prevent port collision.
+process.env.XVFB_DISPLAY_NUM = Math.floor(Math.random() * 99999).toString();
 
 export default defineConfig({
   e2e: {
     specPattern: ["cli_test.cy.ts"],
     supportFile: false,
     setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser, launchOptions) => {
-        launchOptions.args.push("--disable-gpu-shader-disk-cache")
-      })
-    }
+      on("before:browser:launch", (browser, launchOptions) => {
+        launchOptions.args.push("--disable-gpu-shader-disk-cache");
+      });
+    },
   },
-})
+});
--- e2e/workspace/module_test/cypress.config.js
+++ e2e/workspace/module_test/cypress.config.js
@@ -1,13 +1,16 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
+
+// Set XVFB_DISPLAY_NUM to instruct cypress what port to use during CI and prevent port collision.
+process.env.XVFB_DISPLAY_NUM = Math.floor(Math.random() * 99999).toString();
 
 module.exports = defineConfig({
   e2e: {
     specPattern: ["*.cy.js"],
     supportFile: false,
-    setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        launchOptions.args.push("--disable-gpu-shader-disk-cache")
-      })
-    }
+    setupNodeEvents(on, _config) {
+      on("before:browser:launch", (_browser, launchOptions) => {
+        launchOptions.args.push("--disable-gpu-shader-disk-cache");
+      });
+    },
   },
-})
+});
